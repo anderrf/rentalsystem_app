3e4fa29247cf08c3222a273393873b4f08e0a74d
anderrf <andersonrochafarias@gmail.com>
2019-09-04 00:35:33 +0900

    Project's Initial Commit

---
+++ www/components/acao.js
@@ -0,0 +1,9 @@
+// This is a JavaScript file
+
+$(document).on("click", "#btnEntradaInicial", function(){
+  $(location).attr("href", "login.html");
+});
+
+$(document).on("click", "#btnEntradaLogin", function(){
+  $(location).attr("href", "menu.html");
+});
