85984db88b51bb03916665f38f2eb5d0e24230df
anderrf <andersonrochafarias@gmail.com>
2019-10-23 09:28:33 +0900

    listagem, desabilitação e pesquisa de cliente

--- www/components/cliente.js
+++ www/components/cliente.js
@@ -1,43 +1,99 @@
 // This is a JavaScript file
 
-function setModal(valRegCliente)
-{
-  var idCliente = valRegCliente;
-  $("#inpNome").val(nome);
-  $("#inpTelefone").val(telefone);
-  $("#inpEndereco").val(endereco);
+function listarCliente() {
+  $.ajax({
+    type: "post",
+    url: "https://rentalsystempm.000webhostapp.com/cliente/listarClienteBasico.php",
+    dataType: "json",
+    success: function (data) {
+      var regCliente = "";
+      $.each(data.cliente, function (i, dados) {
+        regCliente += "<div class='row linha itemProd' name='regcliente' data-id='" + dados.codigo + "' data-toggle='modal' data-target='#modalCliente'><div class='col-xs-12'><label for='' id='lblNome'><strong>Nome: </strong><br>" + dados.nome + "</label><br><label for='' id='lblTelefone'><strong>Telefone: </strong><br>" + dados.telefone + "</label><br><label for='' id='lblEndereco'><strong>Endereço: </strong><br>" + dados.endereco + "</label><br><label for='' id='lblEmail'><strong>E-mail: </strong><br>" + dados.email + "</label><br></div></div>";
+      });
+      $("#telaCliente").html(regCliente);
+    },
+    error: function (data) {
+      navigator.notification.alert(data);
+    }
+  });
 }
 
-function listarCliente()
-{
-  var regCliente = "";
-  regCliente += "<div class='row linha itemProd' name='regcliente' onclick='setModal(this.value)' value='' data-toggle='modal' data-target='#modalCliente'><div class='col-xs-12'><label for='' id='lblNome'>Nome:</label><br><label for='' id='lblTelefone'>Telefone:</label><br><label for='' id='lblEndereco'>Endereço:</label><br></div></div>";
-  $("#telaCliente").html(regCliente);
-}
-
-$(document).on("click", ".itemProd", function()
-{
+$(document).on("click", ".itemProd", function () {
+  var codCliente = $(this).data('id');
   document.getElementById('hCliente').textContent = "Cliente:";
   var contCliente = "";
-  contCliente += "<div class='row linha'><div class='col-xs-12'><label for=''>Nome:</label><input class='form-control' type='text' readonly id='inpNome'></div></div><div class='row linha'><div class='col-xs-12'><label for=''>Telefone:</label><input class='form-control' type='text' readonly id='inpTelefone'></div></div><div class='row linha'><div class='col-xs-12'><label for=''>Endereço:</label><input class='form-control' type='text' readonly id='inpEndereco'></div></div>";
+  contCliente += "<div class='row linha'><div class='col-xs-12'><label for=''>Nome:</label><input class='form-control' type='text' readonly id='inpNome'></div></div><div class='row linha'><div class='col-xs-12'><label for=''>Telefone:</label><input class='form-control' type='text' readonly id='inpTelefone'></div></div><div class='row linha'><div class='col-xs-12'><label for=''>Endereço:</label><input class='form-control' type='text' readonly id='inpEndereco'></div></div><div class='row linha'><div class='col-xs-12'><label for=''>E-mail:</label><input class='form-control' type='text' readonly id='inpEmail'></div></div>";
   $("#moInner").html(contCliente);
   var ftCliente = "";
-  ftCliente += "<button type='button' class='btn btn-danger' id='btnDeletar'>Deletar</button>";
+  ftCliente += "<button type='button' class='btn btn-danger' id='btnDeletar' onclick='modDeletar(" + codCliente + ")'>Deletar</button>";
   $("#moFooter").html(ftCliente);
-
+  setModal(codCliente);
 });
 
-$(document).on("click", "#btnDeletar", function()
-{
+function setModal(codCliente) {
+  $.ajax({
+    type: "post",
+    url: "https://rentalsystempm.000webhostapp.com/cliente/mostrarClienteBasico.php",
+    data: "codCliente=" + codCliente,
+    dataType: "json",
+    success: function (data) {
+      $("#inpNome").val(data.cliente.nome);
+      $("#inpTelefone").val(data.cliente.telefone);
+      $("#inpEndereco").val(data.cliente.endereco);
+      $("#inpEmail").val(data.cliente.email);
+    },
+    error: function (data) {
+
+    }
+  });
+}
+
+function modDeletar(codCliente) {
   document.getElementById('hCliente').textContent = "Deseja mesmo deletar o cliente?";
   var contDeletar = "";
   contDeletar += "<div class='row linha'><div class='col-xs-12'><label for=''>Confirme a sua senha:</label><input class='form-control' type='text' id='senhaDeletarCliente'></div></div>";
   $("#moInner").html(contDeletar);
   var ftDeletar = "";
-  ftDeletar += "<button type='button' class='btn btn-success' id='btnConfirmarDeletar'>Confirmar</button><button type='button' class='btn btn-danger' data-dismiss='modal'>Cancelar</span></button>";
+  ftDeletar += "<button type='button' class='btn btn-success' id='btnConfirmarDeletar' onclick='confirmarDeletar(" + codCliente + ")'>Confirmar</button><button type='button' class='btn btn-danger' data-dismiss='modal'>Cancelar</span></button>";
   $("#moFooter").html(ftDeletar);
-});
+};
 
-$(document).on("click", "#btnPesquisarCliente", function(){
+function confirmarDeletar(codCliente) {
+  $.ajax({
+    type: "post",
+    url: "https://rentalsystempm.000webhostapp.com/cliente/desabilitarCliente.php",
+    data: "codigo=" + codCliente,
+    success: function (data) {
+      location.reload();
+    },
+    error: function (data) {
+      navigator.notification.alert(data);
+    }
+  });
+}
+
+$(document).on("click", "#btnPesquisarCliente", function () {
+  var pesquisa = $("#inpPesquisarCliente").val();
+  if (pesquisa.length > 3) {
+    $.ajax({
+      type: "post",
+      url: "https://rentalsystempm.000webhostapp.com/cliente/pesquisarClienteBasico.php",
+      data: "pesquisa=" + pesquisa,
+      dataType: "json",
+      success: function (data) {
+        var regCliente = "";
+        $.each(data.cliente, function (i, dados) {
+          regCliente += "<div class='row linha itemProd' name='regcliente' data-id='" + dados.codigo + "' data-toggle='modal' data-target='#modalCliente'><div class='col-xs-12'><label for='' id='lblNome'><strong>Nome: </strong><br>" + dados.nome + "</label><br><label for='' id='lblTelefone'><strong>Telefone: </strong><br>" + dados.telefone + "</label><br><label for='' id='lblEndereco'><strong>Endereço: </strong><br>" + dados.endereco + "</label><br><label for='' id='lblEmail'><strong>E-mail: </strong><br>" + dados.email + "</label><br></div></div>";
+        });
+        $("#telaCliente").html(regCliente);
+      },
+      error: function (data) {
+        navigator.notification.alert(data);
+      }
+    });
+  }
+  else {
+    navigator.notification.alert("Conteúdo digitado é insuficiente para a pesquisa.")
+  }
 
 });
--- www/components/estoque.js
+++ www/components/estoque.js
@@ -1,19 +1,18 @@
 // This is a JavaScript file
 
-$(document).on("click", "#linkMenu", function()
-{
+$(document).on("click", "#linkMenu", function () {
   $(location).attr("href", "menu.html");
 });
 
 //Botão de confirmar cadastro
-$(document).on("click", "#btnCadProd", function(){
+$(document).on("click", "#btnCadProd", function () {
   var prop = document.getElementById('foto').files[0];
   var nome_imagem = prop.name;
   var extensao_imagem = nome_imagem.split('.').pop().toLowerCase();
 
-  if(jQuery.inArray(extensao_imagem, ['png', 'jpg', 'jpeg']) == -1){
+  if (jQuery.inArray(extensao_imagem, ['png', 'jpg', 'jpeg']) == -1) {
     navigator.notification.alert("Imagem inválida");
-  }else{
+  } else {
 
     var form_data = new FormData();
     form_data.append("foto", prop);
@@ -23,13 +22,13 @@ $(document).on("click", "#btnCadProd", function(){
     form_data.append("valor", $("#valorCadProd").val());
 
     $.ajax({
-      url:"https://rentalsystempm.000webhostapp.com/estoque/cadProduto.php",
+      url: "https://rentalsystempm.000webhostapp.com/estoque/cadProduto.php",
       method: "post",
       data: form_data,
       contentType: false,
       cache: false,
       processData: false,
-      success: function(data){
+      success: function (data) {
         navigator.notification.alert(data);
         location.reload();
       }
@@ -38,35 +37,35 @@ $(document).on("click", "#btnCadProd", function(){
 });
 
 //Lista os produtos cadastrados na tela
-function listarProduto(){
+function listarProduto() {
   $.ajax({
-        type:"post",
-        url:"https://rentalsystempm.000webhostapp.com/estoque/listarProduto.php",
-        dataType:"json",
-        //se der certo
-        success: function(data){
-            var itemProduto = "";
-            $.each(data.produto,function(i,dados){
-              itemProduto += 
-              "<div class='row linha itemProd' data-toggle='modal' data-target='#modalProduto' data-id='"+dados.codigo+"'><div class='col-xs-3'><img class='img-responsive' src='https://rentalsystempm.000webhostapp.com/"+dados.foto+"' alt='' style='max-width: 100%; text-align: center'></div><div class='col-xs-9'><div class='row'><div class='col-xs-6'><label for=''><strong>Nome:</strong><br> "+dados.produto+"</label></div><div class='col-xs-6'><label for=''><strong>Descrição:</strong><br> "+dados.descricao+"</label></div></div><div class='row'><div class='col-xs-6'><label for=''><strong>Quantidade:</strong><br> "+dados.quantidade+"</label></div><div class='col-xs-6'><label for=''><strong>Valor:</strong><br> R$ "+dados.valor+"</label></div></div></div></div>";
-            });
-        $("#telaProd").html(itemProduto);
-        },
-        //se der errado
-        error: function(data){
-             navigator.notification.alert(data);
-        }
-    });
+    type: "post",
+    url: "https://rentalsystempm.000webhostapp.com/estoque/listarProduto.php",
+    dataType: "json",
+    //se der certo
+    success: function (data) {
+      var itemProduto = "";
+      $.each(data.produto, function (i, dados) {
+        itemProduto +=
+          "<div class='row linha itemProd' data-toggle='modal' data-target='#modalProduto' data-id='" + dados.codigo + "'><div class='col-xs-3'><img class='img-responsive' src='https://rentalsystempm.000webhostapp.com/" + dados.foto + "' alt='' style='max-width: 100%; text-align: center'></div><div class='col-xs-9'><div class='row'><div class='col-xs-6'><label for=''><strong>Nome:</strong><br> " + dados.produto + "</label></div><div class='col-xs-6'><label for=''><strong>Descrição:</strong><br> " + dados.descricao + "</label></div></div><div class='row'><div class='col-xs-6'><label for=''><strong>Quantidade:</strong><br> " + dados.quantidade + "</label></div><div class='col-xs-6'><label for=''><strong>Valor:</strong><br> R$ " + dados.valor + "</label></div></div></div></div>";
+      });
+      $("#telaProd").html(itemProduto);
+    },
+    //se der errado
+    error: function (data) {
+      navigator.notification.alert(data);
+    }
+  });
 }
 
-function habilita(){
+function habilita() {
   $("#nomeConsProd").prop("readonly", false);
   $("#descConsProd").prop("readonly", false);
   $("#valorConsProd").prop("readonly", false);
   $("#altFoto").prop("hidden", false);
 }
 
-function desabilita(){
+function desabilita() {
   $("#nomeConsProd").prop("readonly", true);
   $("#descConsProd").prop("readonly", true);
   $("#valorConsProd").prop("readonly", true);
@@ -74,8 +73,7 @@ function desabilita(){
 }
 
 //Abre a modal de cadastro de produto
-$(document).on("click", "#addProduto", function()
-{
+$(document).on("click", "#addProduto", function () {
   document.getElementById('hProduto').textContent = "Cadastre o produto:";
   var contCadProduto = "";
   contCadProduto += "<div class='row linha'><div class='col-xs-12'><label for=''>Nome:</label><input class='form-control' type='text' id='nomeCadProd'></div></div><div class='row linha'><div class='col-xs-12'><label for=''>Descrição:</label><input class='form-control' type='text'  id='descCadProd'></div></div><div class='row linha'><div class='col-xs-6'><label for=''>Quantidade:</label><input class='form-control' type='number'  id='quantCadProd'></div><div class='col-xs-6'><label for=''>Valor (por unidade):</label><input class='form-control' type='number'  id='valorCadProd'></div></div><div class='row linha'><div class='col-xs-12'><label for=''>Foto:</label><input class='form-control' type='file' id='foto'></div></div>";
@@ -86,8 +84,7 @@ $(document).on("click", "#addProduto", function()
 });
 
 //Abre a modal de consulta
-$(document).on("click", ".itemProd", function()
-{
+$(document).on("click", ".itemProd", function () {
   var codProduto = $(this).data('id');
   document.getElementById('hProduto').textContent = "Produto:";
   var contConsProduto = "";
@@ -95,37 +92,36 @@ $(document).on("click", ".itemProd", function()
   $("#moInner").html(contConsProduto);
   mostrarProduto(codProduto);
   var ftConsProduto = "";
-  ftConsProduto += "<button type='button' class='btn btn-success' id='btnAltQtProduto' onclick='btnAltQtProduto("+codProduto+")'>Alterar quantidade</button><button type='button' class='btn btn-primary' id='btnAltProd' onclick='btnAltProd()'>Editar</button><button type='button' class='btn btn-danger' data-toggle='modal' data-target='#modalDeletarProd' id='btnDeletarProduto' onclick='btnDeletarProduto()'>Deletar</button>";
+  ftConsProduto += "<button type='button' class='btn btn-success' id='btnAltQtProduto' onclick='btnAltQtProduto(" + codProduto + ")'>Alterar quantidade</button><button type='button' class='btn btn-primary' id='btnAltProd' onclick='btnAltProd()'>Editar</button><button type='button' class='btn btn-danger' data-toggle='modal' data-target='#modalDeletarProd' id='btnDeletarProduto' onclick='btnDeletarProduto()'>Deletar</button>";
   $("#moFooter").html(ftConsProduto);
-  
+
   desabilita();
-  
+
 });
 
 //Mostra as informações do produto selecionado
-function mostrarProduto(codProduto){
+function mostrarProduto(codProduto) {
   $.ajax({
     type: "post",
     url: "https://rentalsystempm.000webhostapp.com/estoque/mostrarProduto.php",
-    data: "codProduto="+codProduto,
+    data: "codProduto=" + codProduto,
     dataType: "json",
-    success: function(data){
+    success: function (data) {
       $("#codigoConsProd").val(data.produto.codigo);
       $("#nomeConsProd").val(data.produto.produto);
       $("#descConsProd").val(data.produto.descricao);
       $("#quantConsProd").val(data.produto.quantidade);
       $("#valorConsProd").val(data.produto.valor);
-      $("#fotoConsProd").attr("src", "https://rentalsystempm.000webhostapp.com/"+data.produto.foto);
+      $("#fotoConsProd").attr("src", "https://rentalsystempm.000webhostapp.com/" + data.produto.foto);
     },
-    error: function(data){
+    error: function (data) {
       navigator.notification.alert(data);
     }
   });
 }
 
 //Botão de alteração de produto
-function btnAltProd()
-{
+function btnAltProd() {
   habilita();
   var ftAlterarProduto = "";
   ftAlterarProduto += "<button type='button' class='btn btn-success' id='btnSalvarAlterar' onclick='salvarAlterar()'>Salvar</button><button type='button' class='btn btn-danger' data-dismiss='modal'>Cancelar</button>";
@@ -133,33 +129,33 @@ function btnAltProd()
 };
 
 //Abre modal de alterar quantidade de produto
-function btnAltQtProduto(codProduto){
+function btnAltQtProduto(codProduto) {
   document.getElementById('hProduto').textContent = "Aumente/diminua a quantidade:";
   var contAltQTProduto = "";
   contAltQTProduto += "<div class='row linha'><div class='col-xs-12'><label for=''>Quantidade a ser adicionada/removida:</label><input class='form-control' type='number' min='1' max='99' maxlength='2' id='inpAltQtProduto'></div></div>";
   $("#moInner").html(contAltQTProduto);
   var ftAltQTProduto = "";
-  ftAltQTProduto += "<button type='button' class='btn btn-success' id='btnAddQtProduto' onclick='addQtProduto("+codProduto+")'>Aumentar</button><button type='button' class='btn btn-warning' id='btnMinQtProduto' onclick='minQtProduto("+codProduto+")'>Diminuir</button><button type='button' class='btn btn-danger' data-dismiss='modal'>Cancelar</button>";
+  ftAltQTProduto += "<button type='button' class='btn btn-success' id='btnAddQtProduto' onclick='addQtProduto(" + codProduto + ")'>Aumentar</button><button type='button' class='btn btn-warning' id='btnMinQtProduto' onclick='minQtProduto(" + codProduto + ")'>Diminuir</button><button type='button' class='btn btn-danger' data-dismiss='modal'>Cancelar</button>";
   $("#moFooter").html(ftAltQTProduto);
 };
 
 //Botão de aumentar quantidade de produto
-function addQtProduto(codProduto){
-  if($("#inpAltQtProduto").val() == ''){
+function addQtProduto(codProduto) {
+  if ($("#inpAltQtProduto").val() == '') {
   }
-  else{
+  else {
     var qtAddProduto = $("#inpAltQtProduto").val();
     var form_data = new FormData();
     form_data.append("codProduto", codProduto);
     form_data.append("qtAddProduto", qtAddProduto);
     $.ajax({
       type: "post",
-      url:"https://rentalsystempm.000webhostapp.com/estoque/addQtProduto.php",
+      url: "https://rentalsystempm.000webhostapp.com/estoque/addQtProduto.php",
       data: form_data,
       contentType: false,
-      cache : false,
+      cache: false,
       processData: false,
-      success: function(data){
+      success: function (data) {
         navigator.notification.alert(data);
         location.reload();
       }
@@ -168,22 +164,22 @@ function addQtProduto(codProduto){
 }
 
 //Botão de diminuir quantidade de produto
-function minQtProduto(codProduto){
-  if($("#inpAltQtProduto").val() == ''){
+function minQtProduto(codProduto) {
+  if ($("#inpAltQtProduto").val() == '') {
   }
-  else{
+  else {
     var qtAddProduto = $("#inpAltQtProduto").val();
     var form_data = new FormData();
     form_data.append("codProduto", codProduto);
     form_data.append("qtAddProduto", qtAddProduto);
     $.ajax({
       type: "post",
-      url:"https://rentalsystempm.000webhostapp.com/estoque/minQtProduto.php",
+      url: "https://rentalsystempm.000webhostapp.com/estoque/minQtProduto.php",
       data: form_data,
       contentType: false,
-      cache : false,
+      cache: false,
       processData: false,
-      success: function(data){
+      success: function (data) {
         navigator.notification.alert(data);
         location.reload();
       }
@@ -191,15 +187,14 @@ function minQtProduto(codProduto){
   }
 }
 
-function salvarAlterar()
-{
+function salvarAlterar() {
   var prop = document.getElementById('novaFoto').files[0];
   var nome_imagem = prop.name;
   var extensao_imagem = nome_imagem.split('.').pop().toLowerCase();
 
-  if(jQuery.inArray(extensao_imagem, ['png', 'jpg', 'jpeg']) == -1){
+  if (jQuery.inArray(extensao_imagem, ['png', 'jpg', 'jpeg']) == -1) {
     navigator.notification.alert("Imagem inválida");
-  }else{
+  } else {
 
     var form_data = new FormData();
     form_data.append("foto", prop);
@@ -209,13 +204,13 @@ function salvarAlterar()
     form_data.append("valor", $("#valorConsProd").val());
 
     $.ajax({
-      url:"https://rentalsystempm.000webhostapp.com/estoque/alterarProduto.php",
+      url: "https://rentalsystempm.000webhostapp.com/estoque/alterarProduto.php",
       method: "post",
       data: form_data,
       contentType: false,
       cache: false,
       processData: false,
-      success: function(data){
+      success: function (data) {
         navigator.notification.alert(data);
         location.reload();
       }
@@ -224,29 +219,27 @@ function salvarAlterar()
 }
 
 //Abre a modal de deletar produto
-function btnDeletarProduto()
-{
+function btnDeletarProduto() {
   var codProduto = $("#codigoConsProd").val();
   document.getElementById('hProduto').textContent = "Deseja mesmo excluir este produto?";
   var contDeletarProduto = "";
   contDeletarProduto += "<div class='row linha'><div class='col-xs-12'><label for=''>Caso sim, insira a sua senha:</label><input class='form-control' type='text' id='inpSenhaDeletarProduto'></div></div>";
   $("#moInner").html(contDeletarProduto);
   var ftDeletarProduto = "";
-  ftDeletarProduto += "<button type='button' class='btn btn-success' id='btnConfirmaDeletarProduto' onclick='btnConfirmaDeletarProduto("+codProduto+")'>Deletar</button><button type='button' class='btn btn-danger' data-dismiss='modal'>Cancelar</button>";
+  ftDeletarProduto += "<button type='button' class='btn btn-success' id='btnConfirmaDeletarProduto' onclick='btnConfirmaDeletarProduto(" + codProduto + ")'>Deletar</button><button type='button' class='btn btn-danger' data-dismiss='modal'>Cancelar</button>";
   $("#moFooter").html(ftDeletarProduto);
 }
 
-function btnConfirmaDeletarProduto(codProduto)
-{
+function btnConfirmaDeletarProduto(codProduto) {
   $.ajax({
     type: "post",
     url: "https://rentalsystempm.000webhostapp.com/estoque/deletarProduto.php",
-    data: "codProduto="+codProduto,
-    success: function(data){
+    data: "codProduto=" + codProduto,
+    success: function (data) {
       navigator.notification.alert(data);
       location.reload();
     },
-    error: function(data){
+    error: function (data) {
       navigator.notification.alert(data);
     }
   });
